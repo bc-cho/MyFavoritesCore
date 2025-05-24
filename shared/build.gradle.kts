db1918cb31f639ab953055fe7024a5032b710994
ByungchulJo <bc.cho@outlook.jp>
2025-05-24 12:23:23 +0900

    Java Versionを17に上げる

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
         compilations.all {
             compileTaskProvider.configure {
                 compilerOptions {
-                    jvmTarget.set(JvmTarget.JVM_1_8)
+                    jvmTarget.set(JvmTarget.JVM_17)
                 }
             }
         }
@@ -54,8 +54,8 @@ android {
         minSdk = 24
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
