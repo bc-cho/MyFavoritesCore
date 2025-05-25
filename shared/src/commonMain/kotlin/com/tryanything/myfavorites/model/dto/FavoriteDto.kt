880ec36a74d3418a5c1f9808e1af3f3d8168badd
ByungchulJo <bc.cho@outlook.jp>
2025-05-25 20:41:37 +0900

    Entity, DTO追加

---
+++ shared/src/commonMain/kotlin/com/tryanything/myfavorites/model/dto/FavoriteDto.kt
@@ -0,0 +1,19 @@
+package com.tryanything.myfavorites.model.dto
+
+import com.tryanything.myfavorites.model.entity.FavoritePlaceEntity
+
+class FavoriteDto(
+    val name: String,
+    val address: String,
+    val imageUrl: String? = null,
+    val lat: Double,
+    val lon: Double
+) {
+    constructor(item: FavoritePlaceEntity) : this(
+        name = item.name,
+        address = item.address,
+        imageUrl = item.imageUrl,
+        lat = item.latitude,
+        lon = item.longitude
+    )
+}
