136cfc8aa4858afbe11bda31bb639311697d9fe4
ByungchulJo <bc.cho@outlook.jp>
2025-05-26 12:29:24 +0900

    HttpClientEngineをそれぞれに定義

---
+++ shared/src/androidMain/kotlin/com/tryanything/myfavorites/network/HttpClient.kt
@@ -0,0 +1,6 @@
+package com.tryanything.myfavorites.network
+
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+
+actual fun getHttpClientEngine(): HttpClientEngine = OkHttp.create { }
