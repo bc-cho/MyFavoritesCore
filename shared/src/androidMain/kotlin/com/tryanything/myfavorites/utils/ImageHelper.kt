e0a15c202917554d976574f90de112e7d335fb82
ByungchulJo <bc.cho@outlook.jp>
2025-05-26 12:42:08 +0900

    Google ImageUrl変換メソッド追加

---
+++ shared/src/androidMain/kotlin/com/tryanything/myfavorites/utils/ImageHelper.kt
@@ -0,0 +1,17 @@
+package com.tryanything.myfavorites.utils
+
+object ImageHelper {
+    /**
+     * Google Place APIの指定されたPhotoNameをリクエスト可能なURLに変換する
+     */
+    fun changeToGooglePhotoUrl(
+        name: String,
+        apiKey: String,
+        maxWidth: Int = PHOTO_MAX_WIDTH_PX,
+        maxHeight: Int = PHOTO_MAX_HEIGHT_PX
+    ): String =
+        "https://places.googleapis.com/v1/$name/media?key=$apiKey&maxHeightPx=$maxHeight&maxWidthPx$maxWidth"
+    
+    private const val PHOTO_MAX_WIDTH_PX: Int = 200
+    private const val PHOTO_MAX_HEIGHT_PX: Int = 200
+}
